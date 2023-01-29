b37416bcd0ad16ce1ebe4179677fdf0f121aeacf
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-29 16:49:08 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Info = () => {
-
+    const navigate = useNavigate()
 
     return (
         <div id="info-page">
 
-
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
-             <button id="info-to-blog"> Go to Blog</button>
-            <button id="info-to-home"> Go to Home</button>
+            <button id="info-to-blog" onClick={() => navigate('/blog')}> Go to Blog</button>
+            <button id="info-to-home" onClick={() => navigate('/')}> Go to Home</button>
         </div>
     )
 }
